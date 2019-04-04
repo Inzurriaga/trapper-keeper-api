c4993f6849d4a782a8d6eda45678da14c2b68e38
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-04-04 16:03:08 -0600

    delete

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.get("/api/v1/notes", (request, response) => {
     response.status(200).json(app.locals.notes)
 })
 
-app.get("/api/v1/notes/:id", (request, response) => {
-
-})
-
 app.post("/api/v1/notes", (request, response) => {
     const { notes } = app.locals
     const newNote = request.body
@@ -29,12 +25,20 @@ app.patch("/api/v1/notes/:id", (request, response) => {
 
 })
 
-app.put("/api/v1/notes/:id", (request, response) => {
-
-})
+// app.put("/api/v1/notes/:id", (request, response) => {
+// 	const { id } = request.params;
+// 	const updatedNotes =
+// })
 
 app.delete("/api/v1/notes/:id", (request, response) => {
-
+		console.log('backend delete fires')
+		console.log(request.params.id)
+		const { id } = request.params
+		const updatedNotes = app.locals.notes.filter(note => note.id != id);
+		if(updatedNotes === app.locals.notes) return response.sendStatus(422)
+		console.log("im working")
+		app.locals.notes = updatedNotes
+		return response.sendStatus(204)
 })
 
 export default app;
